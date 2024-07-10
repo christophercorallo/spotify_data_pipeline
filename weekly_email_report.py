c4782cec8c9c95501e97817e7d40f4f97e9e2065
Chris <73146262+christophercorallo@users.noreply.github.com>
2024-07-10 18:13:05 -0400

    add top weekly funcs
    Add functions that fetch the top artists and songs from the past week

--- weekly_email_report.py
+++ weekly_email_report.py
@@ -7,6 +7,7 @@ from googleapiclient.errors import HttpError
 import base64
 from email.message import EmailMessage
 import cred
+from spotify_etl import connect_to_snowflake
 
 # Define scope
 SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
@@ -60,6 +61,61 @@ def send_email(creds):
     # print error message if any (hopefully not)
     print(f"An error occurred: {error}")
   
+def fetch_top_weekly_songs(con, close_connection = True):
+  # query data using connection
+  try:
+    cur = con.cursor()
+    cur.execute(
+      ("with last_weeks_songs as ("
+      "select distinct * "
+      "from SPOTIFY_DATA.SPOTIFY_RECENTLY_PLAYED.SPOTIFY_RECENTLY_PLAYED "
+      "WHERE \"PLAYED_AT\" >= current_date - interval '7 days' "
+      "order by played_at desc"
+      ") "
+      "select song_name, count(song_name) as total "
+      "from last_weeks_songs "
+      "group by 1 "
+      "order by total desc "
+      "limit 5;"
+      )
+    )
+    for (col1, col2) in cur:
+        print('{0}, {1}'.format(col1, col2))
+
+  finally:
+    if close_connection:
+      # close connection
+      con.close()
+
+def fetch_top_weekly_artists(con, close_connection = True):
+  # query data using connection
+  try:
+    cur = con.cursor()
+    cur.execute(
+      ("with last_weeks_artists as ("
+      "select distinct * "
+      "from SPOTIFY_DATA.SPOTIFY_RECENTLY_PLAYED.SPOTIFY_RECENTLY_PLAYED "
+      "WHERE \"PLAYED_AT\" >= current_date - interval '7 days' "
+      "order by played_at desc"
+      ") "
+      "select artist_name, count(artist_name) as total "
+      "from last_weeks_artists "
+      "group by 1 "
+      "order by total desc "
+      "limit 5;"
+      )
+    )
+    for (col1, col2) in cur:
+        print('{0}, {1}'.format(col1, col2))
+
+  finally:
+    if close_connection:
+      # close connection
+      con.close()
+
 if __name__ == "__main__":
   creds = connect_to_gmail_api(SCOPES)
-  send_email(creds)
+  # send_email(creds)
+  # con = connect_to_snowflake()
+  # fetch_top_weekly_songs(con, close_connection=False)
+  # fetch_top_weekly_artists(con)

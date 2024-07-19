b99b5f2f0fd98d2239c72067a269a830c75be7eb
Chris <73146262+christophercorallo@users.noreply.github.com>
2024-07-18 22:54:38 -0400

    improve write_to_postgres
    add duplicate check, use psycopg to insert data instead of sqlalchemy, cleaned up and commented code

--- write_data_to_postgres.py
+++ write_data_to_postgres.py
@@ -2,7 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred
 import pandas as pd
-from sqlalchemy import create_engine
+import psycopg2
+import psycopg2.extras as extras
+import datetime
 
 def connect_to_spotify(scope):
     # authenticate api
@@ -36,13 +38,48 @@ def load_songs_to_df(songs):
 
     return song_df
 
-def write_data_to_postgres(song_df):
-    # connect to postgres
-    con = create_engine(cred.postgres_url)
-    # write songs to postgres
-    song_df.to_sql('recently_played', con, schema="spotify_user_data",if_exists="append", index=False)
+def connect_to_postgres():
+    # connect to postgres database using psycopg
+    conn = psycopg2.connect(database = "spotify_data", 
+                    user = cred.postgres_username, 
+                    host= 'localhost',
+                    password = cred.postgres_password,
+                    port = 5432,
+                    options="-c search_path=spotify_user_data")
+    return conn # return connection, NOT CURSOR
 
+def remove_duplicates(con,song_df):
+    # initialize cursor
+    cur = con.cursor()
+    # obtain most recent song in db
+    cur.execute("select \"PLAYED_AT\" from SPOTIFY_USER_DATA.\"recently_played\" order by \"PLAYED_AT\" desc limit 1")
+    most_recent_str = cur.fetchall()
+    most_recent = datetime.datetime.strptime(most_recent_str[0][0], '%Y-%m-%dT%H:%M:%S.%fZ')
+    # remove any songs obtained from spotify api after most_recent
+    for id, date in enumerate(song_df['PLAYED_AT']):
+        current_date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        if current_date <= most_recent:
+            song_df.drop(id, inplace = True)
+
+def write_data_to_postgres(con,song_df):
+    # initialize cursor
+    cur = con.cursor()
+    # generate query to insert data
+    tableName = 'recently_played'
+    dataset = [tuple(x) for x in song_df.to_numpy()]
+    cols = "\"SONG_NAME\",\"ARTIST_NAME\",\"PLAYED_AT\""
+    query  = "INSERT INTO %s(%s) VALUES %%s" % (tableName, cols)
+    
+    # insert data
+    try:
+        extras.execute_values(cur,query,dataset)
+        con.commit()
+    finally:
+        # close cursor
+        cur.close()
+    
 recent_songs = connect_to_spotify('user-read-recently-played')
 recent_songs_df = load_songs_to_df(recent_songs)
-write_data_to_postgres(recent_songs_df)
-
+connection = connect_to_postgres()
+remove_duplicates(connection,recent_songs_df)
+write_data_to_postgres(connection,recent_songs_df)

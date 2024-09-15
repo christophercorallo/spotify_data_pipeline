e9f7443a2cbb521e7667aec807801a3b7dad62e0
Chris <73146262+christophercorallo@users.noreply.github.com>
2024-09-15 16:21:22 -0400

    make dag more modular
    edit dag making it more modular

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -1,20 +1,38 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from write_data_to_postgres import write_data_to_postgres
+from write_data_to_postgres import connect_to_spotify, load_songs_to_df, connect_to_postgres, write_data_to_postgres
 
 default_args = {
-    'owner': 'airscholar',
+    'owner': 'airflow',
     'start_date': datetime(2024, 8, 15, 00, 00)
 }
+dag = DAG(
+    dag_id='python_operator', default_args=default_args,
+    schedule_interval='0 */3 * * *')
+
+songs = PythonOperator(
+    task_id='connect to spotify',
+    python_callable=connect_to_spotify(),
+    op_kwargs = {'scope': 'user-read-recently-played'},
+    dag=dag)
+
+songs_df = PythonOperator(
+    task_id='load songs from spotify',
+    python_callable=load_songs_to_df(),
+    op_kwargs = {'songs': songs},
+    dag=dag)
+
+conn = PythonOperator(
+    task_id='connect to postgres',
+    python_callable=connect_to_postgres(),
+    dag=dag)
+
+write_data = PythonOperator(
+    task_id='write data to postgres',
+    python_callable=write_data_to_postgres(),
+    op_kwargs = {'con': conn, 'song_df': songs_df},
+    dag=dag)
 
 
-with DAG('user_automation',
-         default_args=default_args,
-         schedule_interval='0 */3 * * *',
-         catchup=False) as dag:
-
-    streaming_task = PythonOperator(
-        task_id='write_data_to_db',
-        python_callable=write_data_to_postgres
-    )
+songs >> songs_df >> conn >> write_data
